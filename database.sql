4e2827354683f098a83fc94b9b0b057b6d930d49
thomasangelop <me@thomasagnelo.codes>
2019-07-18 16:00:52 -0500

    file structure

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
 CREATE TABLE "person" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "favoriteLocations" VARCHAR, 
+    "favoriteWhiskies" VARCHAR
 );
