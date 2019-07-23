70d49484caa1621920afb9476c4070917eee9471
thomasangelop <me@thomasagnelo.codes>
2019-07-22 21:36:56 -0500

    marvel bar location added with a few whiskeys available

--- src/components/GoogleMaps/simpleMap.js
+++ src/components/GoogleMaps/simpleMap.js
@@ -5,15 +5,49 @@ const mapStyles = {
   width: '80%',
   height: '80%'
 };
+
 const apiKey = require('../../apiKey');
 
-console.log(apiKey);
+const whiskeyLocations = {
+    marvelBar: {
+        name: 'Marvel Bar',
+        whiskeyAvailable: 'Macallan 12yr, Macallan 15yr',
+        position: {
+            lat: 44.9858,
+            lng: -93.2687
+        }
+    },
+    spoonStable: {
+        name: 'Spoon & Stable',
+        whiskeyAvailable: 'Laphroig 10yr, Johnnie Walker Blue Label',
+        position: {
+            lat: 44.9856,
+            lng: -93.2696
+        }
+    }
+}
 
 export class SimpleMaps extends Component {
   state = {
     showingInfoWindow: false,  //Hides or the shows the infoWindow
     activeMarker: {},          //Shows the active marker upon click
     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+};
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   };
 
   render() {
@@ -26,7 +60,48 @@ export class SimpleMaps extends Component {
          lat: 44.9778,
          lng: -93.2650
         }}
-      />
+      >
+        {/* {whiskeyLocations.map((location, i) => (
+            <Marker 
+                key={i}
+                onClick={this.onMarkerClick}
+                name={location.name}
+                position={{
+                    lat: location.position.lat,
+                    lng: location.position.lng
+                }}
+            />
+        ))}  */}
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Marvel Bar'}
+          position={{
+            lat: 44.9858,
+            lng: -93.2687
+           }}
+           whiskeyAvailable={'Macallan 12yr, Macallan 15yr'}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Minneapolis'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+            <p>Whiskey Available: 
+             <ul>
+              <li>{this.state.selectedPlace.whiskeyAvailable}</li>
+             </ul>
+            </p>
+          </div>
+        </InfoWindow>
+      </Map>
+      //TODO: 'Try and add a few more markers on your map and 
+      //more interactivity to your infoWindow';
     );
   }
 }
